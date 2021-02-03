b47fe9d1c5bab22f8d0597ce157413cb154bed93
paulopez78 <pau.lopez.78@gmail.com>
2021-02-03 14:39:08 +0100

    docker images

---
+++ Meetup/pull.sh
@@ -0,0 +1,9 @@
+docker pull mcr.microsoft.com/dotnet/runtime-deps
+docker pull mcr.microsoft.com/dotnet/runtime
+docker pull mcr.microsoft.com/dotnet/aspnet
+docker pull mcr.microsoft.com/dotnet/sdk
+
+docker pull rabbitmq:management
+docker pull postgres
+docker pull ubuntu
+docker pull debian
