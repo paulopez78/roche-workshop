9240c66c7b3de8a8c6d96cca39afce61705da365
paulopez78 <pau.lopez.78@gmail.com>
2021-02-11 23:15:09 +0100

    start deployment

---
+++ session5/5.deployment/Meetup/MeetupEvents.Test/MeetupEventIntegrationTest.cs
@@ -0,0 +1,241 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using Xunit;
+using Xunit.Abstractions;
+using static System.Guid;
+using MeetupEvents.Contracts;
+using static MeetupEvents.Contracts.MeetupCommands.V1;
+
+namespace MeetupEvents.Test
+{
+    public class MeetupEventIntegrationTest : IClassFixture<WebTestFixture>
+    {
+        public MeetupEventIntegrationTest(WebTestFixture fixture, ITestOutputHelper testOutputHelper)
+        {
+            Fixture        = fixture;
+            Fixture.Output = testOutputHelper;
+            Client         = Fixture.CreateClient();
+        }
+
+        readonly WebTestFixture Fixture;
+        readonly HttpClient     Client;
+
+        [Fact]
+        public async Task Should_Create_Meetup()
+        {
+            // arrange
+            var meetupId = NewGuid();
+
+            // act
+            var response = await CreateMeetup(meetupId);
+
+            // assert 
+            var meetup = await Get(meetupId);
+            meetup.Title.Should().Be(Title);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Not_Duplicate_Meetup()
+        {
+            // arrange
+            var meetupId = NewGuid();
+            await CreateMeetup(meetupId);
+
+            // act
+            var response = await CreateMeetup(meetupId);
+
+            // assert 
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_Publish_Meetup()
+        {
+            // arrange
+            var meetupId = NewGuid();
+            await CreateMeetup(meetupId);
+            await Schedule(meetupId);
+            await MakeOnline(meetupId);
+
+            // act
+            var response = await Publish(meetupId);
+
+            // assert 
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Cancel_Meetup()
+        {
+            // arrange
+            var meetupId = NewGuid();
+            await CreateMeetup(meetupId);
+            await Schedule(meetupId);
+            await MakeOnline(meetupId);
+            await Publish(meetupId);
+
+            // act
+            var response = await Cancel(meetupId);
+
+            // assert 
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Not_Cancel_None_Existing_Meetup()
+        {
+            // arrange
+            var meetupId = NewGuid();
+
+            // act
+            var response = await Cancel(meetupId);
+
+            // assert 
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Should_Produce_Concurrency_Problem_When_Attending()
+        {
+            // arrange
+            var meetupId = NewGuid();
+
+            await CreateMeetup(meetupId);
+            await Schedule(meetupId);
+            await MakeOnline(meetupId);
+            await Publish(meetupId);
+            await ReduceCapacity(meetupId, byNumber: 8);
+
+            // eventual consistency, we should retry till is attendant list is opened
+            await Task.Delay(1000);
+
+            var bob   = NewGuid();
+            var alice = NewGuid();
+            var joe   = NewGuid();
+
+            // act
+            await Task.WhenAll(
+                AttendWithRetry(bob),
+                AttendWithRetry(alice),
+                AttendWithRetry(joe)
+            );
+
+            // assert 
+            var meetup = await Get(meetupId);
+            meetup.Attendants.Should().HaveCount(3);
+            meetup.Attendants.Count(x => x.Waiting).Should().Be(1);
+
+            Task AttendWithRetry(Guid memberId)
+                => Retry().ExecuteAsync(() => Attend(meetupId, memberId));
+        }
+
+        [Fact]
+        public async Task Should_Produce_Concurrency_Problem_When_Attend_And_UpdateDetails()
+        {
+            // arrange
+            var meetupId = NewGuid();
+            await CreateMeetup(meetupId);
+            await Schedule(meetupId);
+            await MakeOnline(meetupId);
+            await Publish(meetupId);
+
+            // eventual consistency, we should retry till is attendant list is opened
+            await Task.Delay(1000);
+
+            var bob   = NewGuid();
+            var title = "Microservices Benefits";
+
+            // act
+            await Task.WhenAll(
+                Attend(meetupId, bob),
+                UpdateDetails(meetupId, title)
+            );
+
+            // assert 
+            var meetup = await Get(meetupId);
+            meetup.Title.Should().Be(title);
+            meetup.Going(bob).Should().BeTrue();
+        }
+
+        const string BaseUrl     = "/api/meetup/events";
+        const string Title       = "Microservices Failures";
+        const string Description = "This is a talk about ...";
+
+        Task<HttpResponseMessage> CreateMeetup(Guid id) =>
+            Client.PostAsJsonAsync(BaseUrl, new Create(id, NewGuid(), Title, Description));
+
+        Task<HttpResponseMessage> UpdateDetails(Guid id, string title, string description = null) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/details", new UpdateDetails(id, title, description ?? Description));
+
+        Task<HttpResponseMessage> Publish(Guid id) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/publish", new Publish(id));
+
+        Task<HttpResponseMessage> Schedule(Guid id)
+        {
+            var now = DateTimeOffset.UtcNow;
+            return Client.PutAsJsonAsync($"{BaseUrl}/schedule",
+                new Schedule(id, now.AddMonths(1), now.AddMonths(1).AddHours(2)));
+        }
+
+        Task<HttpResponseMessage> MakeOnline(Guid id) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/online", new MakeOnline(id, new Uri("https://zoom.us/netcore")));
+
+        Task<HttpResponseMessage> Cancel(Guid id, string reason = "") =>
+            Client.PutAsJsonAsync($"{BaseUrl}/cancel", new Cancel(id, reason));
+
+        Task<HttpResponseMessage> CreateAttendantList(Guid id, Guid meetupId, int capacity = 10) =>
+            Client.PostAsJsonAsync($"{BaseUrl}/attendants",
+                new AttendantListCommands.V1.CreateAttendantList(id, meetupId, capacity));
+
+        Task<HttpResponseMessage> Open(Guid meetupId) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/attendants/open", new AttendantListCommands.V1.Open(meetupId));
+
+        Task<HttpResponseMessage> Attend(Guid meetupId, Guid memberId) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/attendants/attend",
+                new AttendantListCommands.V1.Attend(meetupId, memberId));
+
+        Task<HttpResponseMessage> CancelAttendance(Guid meetupId, Guid memberId) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/attendants/cancel",
+                new AttendantListCommands.V1.CancelAttendance(meetupId, memberId));
+
+        Task<HttpResponseMessage> IncreaseCapacity(Guid meetupId, int byNumber) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/attendants/increase",
+                new AttendantListCommands.V1.IncreaseCapacity(meetupId, byNumber));
+
+        Task<HttpResponseMessage> ReduceCapacity(Guid meetupId, int byNumber) =>
+            Client.PutAsJsonAsync($"{BaseUrl}/attendants/reduce",
+                new AttendantListCommands.V1.ReduceCapacity(meetupId, byNumber));
+
+        Task<ReadModels.V1.MeetupEvent> Get(Guid id) =>
+            Client.GetFromJsonAsync<ReadModels.V1.MeetupEvent>($"/api/meetup/events/{id}");
+
+        AsyncRetryPolicy<HttpResponseMessage> Retry()
+        {
+            Random jitterer = new();
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(jitterer.Next(0, 100)));
+        }
+    }
+
+    public static class MeetupEventsIntegrationTestExtensions
+    {
+        public static bool Going(this ReadModels.V1.MeetupEvent meetup, Guid memberId)
+            => meetup.Attendants.Any(x => x.MemberId == memberId && !x.Waiting);
+
+        public static bool NotGoing(this ReadModels.V1.MeetupEvent meetup, Guid memberId)
+            => meetup.Attendants.All(x => x.MemberId != memberId);
+
+        public static bool Waiting(this ReadModels.V1.MeetupEvent meetup, Guid memberId)
+            => meetup.Attendants.Any(x => x.MemberId == memberId && x.Waiting);
+    }
+}
---
+++ session5/5.deployment/Meetup/MeetupEvents/Application/AttendantListApplicationService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using MeetupEvents.Domain;
+using MeetupEvents.Framework;
+using MeetupEvents.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using static MeetupEvents.Contracts.AttendantListCommands.V1;
+
+namespace MeetupEvents.Application
+{
+    public class AttendantListApplicationService : ApplicationService<AttendantListAggregate>
+    {
+        readonly Func<DateTimeOffset> _getUtcNow;
+        readonly GetMappedId          _getMappedId;
+
+        public AttendantListApplicationService(MeetupEventsDbContext db, Func<DateTimeOffset> getUtcNow,
+            GetMappedId getMappedId
+        ) : base(db)
+        {
+            _getUtcNow   = getUtcNow;
+            _getMappedId = getMappedId;
+        }
+
+        public override Task<CommandResult> Handle(object command) =>
+            command switch
+            {
+                CreateAttendantList create =>
+                    HandleCreate(
+                        create.Id,
+                        attendantList => attendantList.Create(
+                            create.Id,
+                            create.MeetupId,
+                            create.Capacity
+                        )
+                    ),
+
+                IncreaseCapacity increase =>
+                    Handle(
+                        MapId(increase.MeetupId),
+                        attendantList => attendantList.IncreaseCapacity(increase.ByNumber)
+                    ),
+
+                ReduceCapacity reduce =>
+                    Handle(
+                        MapId(reduce.MeetupId),
+                        attendantList => attendantList.ReduceCapacity(reduce.ByNumber)
+                    ),
+
+                Open open =>
+                    Handle(
+                        MapId(open.MeetupId),
+                        attendantList => attendantList.Open(_getUtcNow())
+                    ),
+                Close close =>
+                    Handle(
+                        MapId(close.MeetupId),
+                        attendantList => attendantList.Open(_getUtcNow())
+                    ),
+
+                Archive archive =>
+                    Handle(
+                        MapId(archive.MeetupId),
+                        attendantList => attendantList.Open(_getUtcNow())
+                    ),
+
+                Attend attend =>
+                    Handle(
+                        MapId(attend.MeetupId),
+                        attendantList => attendantList.Attend(attend.MemberId, _getUtcNow())
+                    ),
+
+                CancelAttendance cancelAttendance =>
+                    Handle(
+                        MapId(cancelAttendance.MeetupId),
+                        attendantList => attendantList.CancelAttendance(cancelAttendance.MemberId)
+                    ),
+
+                _ => throw new InvalidOperationException("Command handler does not exist")
+            };
+
+        async Task<Guid> MapId(Guid id)
+        {
+            var mapId = await _getMappedId(id);
+            if (!mapId.HasValue)
+                throw new ArgumentException($"Can not found mapped id {id}");
+
+            return mapId.Value;
+        }
+
+        protected override Task<AttendantListAggregate?> Load(Guid id, DbContext repository) =>
+            repository.Set<AttendantListAggregate>()
+                .Include(x => x.Attendants)
+                .SingleOrDefaultAsync(x => x.Id == id)!;
+    }
+}
