0aff7e6983ef663f5cf1b053e285d39912a176ef
paulopez78 <pau.lopez.78@gmail.com>
2021-02-12 01:07:55 +0100

    remove

--- session5/5.deployment/Meetup/queries.sql
+++
@@ -1,4 +0,0 @@
-SELECT M."Id",M."Status",AL."Id",AL."MeetupEventId", AL."Status",A."MemberId", A."Waiting"
-FROM "MeetupEvent" M
-         LEFT JOIN "AttendantList" AL ON M."Id" = AL."MeetupEventId"
-         LEFT JOIN "Attendant" A on AL."Id" = A."AttendantListAggregateId"
