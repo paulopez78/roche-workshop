c3036312094452a0b43906857ee3d1f025924772
paulopez78 <pau.lopez.78@gmail.com>
2021-02-15 22:16:33 +0100

    add opentelemtry to queries

--- session5-6/6.process-manager/Meetup/MeetupEvents.Queries/Startup.cs
+++ session5-6/6.process-manager/Meetup/MeetupEvents.Queries/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Npgsql;
+using OpenTelemetry.Trace;
+using static MeetupEvents.Queries.Program;
 
 namespace MeetupEvents.Queries
 {
@@ -26,6 +28,15 @@ namespace MeetupEvents.Queries
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "MeetupEvents.Queries", Version = "v1"});
             });
+            
+            services.AddOpenTelemetryTracing(b =>
+                b.AddAspNetCoreInstrumentation()
+                    .AddJaegerExporter(o =>
+                    {
+                        o.ServiceName = ApplicationKey;
+                        o.AgentHost   = Configuration["JAEGER_HOST"] ?? "localhost";
+                    })
+            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
